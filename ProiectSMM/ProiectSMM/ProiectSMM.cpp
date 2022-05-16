40e4d551439e6f374c0d0902bddf5ee8aa8c1595
Idu Sergiu <idu_sergiu@yahoo.com>
2022-05-16 13:28:22 +0300

    Added the duck in the second room

--- ProiectSMM/ProiectSMM/ProiectSMM.cpp
+++ ProiectSMM/ProiectSMM/ProiectSMM.cpp
@@ -88,7 +88,7 @@ void renderStatue1(const Shader& shader);
 void renderStatue2(const Shader& shader);
 void renderGrass(const Shader& shader);
 void renderStork(const Shader& shader);
-
+void renderDuck(const Shader& shader);
 
 //objects
 void renderFloor();
@@ -100,6 +100,7 @@ void renderStatue1();
 void renderStatue2();
 void renderGrass();
 void renderStork();
+void renderDuck();
 
 //room
 void renderWall1();
@@ -190,6 +191,7 @@ int main(int argc, char** argv)
 	unsigned int statue2Texture = CreateTexture(strExePath + "\\statue.jpg");
 	unsigned int GrassTexture = CreateTexture(strExePath + "\\greenGrass.jpg");
 	unsigned int storkTexture = CreateTexture(strExePath + "\\stork.jpg");
+	unsigned int duckTexture = CreateTexture(strExePath + "\\duck.jpg");
 	// configure depth map FBO
 	// -----------------------
 	const unsigned int SHADOW_WIDTH = 4098, SHADOW_HEIGHT = 4098;
@@ -367,7 +369,17 @@ int main(int argc, char** argv)
 		renderStork(shadowMappingDepthShader);
 		glCullFace(GL_BACK);
 		glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
+		
+		glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
+		glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+		glClear(GL_DEPTH_BUFFER_BIT);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, duckTexture);
+		glEnable(GL_CULL_FACE);
+		glCullFace(GL_FRONT);
+		renderDuck(shadowMappingDepthShader);
+		glCullFace(GL_BACK);
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
 		// reset viewport
 		glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
@@ -467,6 +479,13 @@ int main(int argc, char** argv)
 		glDisable(GL_CULL_FACE);
 		renderStork(shadowMappingShader);
 
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, duckTexture);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, depthMap);
+		glDisable(GL_CULL_FACE);
+		renderDuck(shadowMappingShader);
+		
 		//end
 
 
@@ -746,6 +765,22 @@ void renderStork(const Shader& shader)
 	renderStork();
 }
 
+
+void renderDuck(const Shader& shader)
+{
+
+	//duck
+
+	glm::mat4 model;
+	model = glm::mat4();
+	model = glm::translate(model, glm::vec3(-45.0f, 0.8f, 13.9f));
+	model = glm::scale(model, glm::vec3(0.027f));
+	model = glm::rotate(model, glm::radians(180.f), glm::vec3(0.0f, 1.0f, 0.0f));
+	model = glm::rotate(model, glm::radians(50.f), glm::vec3(0.0f, -1.0f, 0.0f));
+	model = glm::rotate(model, glm::radians(10.f), glm::vec3(0.0f, 0.0f, 1.0f));
+	shader.SetMat4("model", model);
+	renderDuck();
+}
 
 unsigned int planeVAO = 0;
 void renderFloor()
@@ -2232,6 +2267,87 @@ void renderStork()
 	glBindVertexArray(0);
 }
 
+unsigned int indicesDuck[72000];
+objl::Vertex verDuck[82000];
+GLuint duckVAO, duckVBO, duckEBO;
+
+void renderDuck()
+{
+	// initialize (if necessary)
+	if (duckVAO == 0)
+	{
+
+		std::vector<float> verticess;
+		std::vector<float> indicess;
+
+
+
+		Loader.LoadFile("..\\OBJ\\duck.obj");
+		objl::Mesh curMesh = Loader.LoadedMeshes[0];
+		int size = curMesh.Vertices.size();
+		objl::Vertex v;
+		for (int j = 0; j < curMesh.Vertices.size(); j++)
+		{
+			v.Position.X = (float)curMesh.Vertices[j].Position.X;
+			v.Position.Y = (float)curMesh.Vertices[j].Position.Y;
+			v.Position.Z = (float)curMesh.Vertices[j].Position.Z;
+			v.Normal.X = (float)curMesh.Vertices[j].Normal.X;
+			v.Normal.Y = (float)curMesh.Vertices[j].Normal.Y;
+			v.Normal.Z = (float)curMesh.Vertices[j].Normal.Z;
+			v.TextureCoordinate.X = (float)curMesh.Vertices[j].TextureCoordinate.X;
+			v.TextureCoordinate.Y = (float)curMesh.Vertices[j].TextureCoordinate.Y;
+
+
+			verDuck[j] = v;
+		}
+		for (int j = 0; j < verticess.size(); j++)
+		{
+			vertices[j] = verticess.at(j);
+		}
+
+		for (int j = 0; j < curMesh.Indices.size(); j++)
+		{
+
+			indicess.push_back((float)curMesh.Indices[j]);
+
+		}
+		for (int j = 0; j < curMesh.Indices.size(); j++)
+		{
+			indicesDuck[j] = indicess.at(j);
+		}
+
+		glGenVertexArrays(1, &duckVAO);
+		glGenBuffers(1, &duckVBO);
+		glGenBuffers(1, &duckEBO);
+		// fill buffer
+		glBindBuffer(GL_ARRAY_BUFFER, duckVBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(verDuck), verDuck, GL_DYNAMIC_DRAW);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, duckEBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicesDuck), &indicesDuck, GL_DYNAMIC_DRAW);
+		// link vertex attributes
+		glBindVertexArray(duckVAO);
+		glEnableVertexAttribArray(0);
+
+
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+		glEnableVertexAttribArray(2);
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindVertexArray(0);
+	}
+	// render Cube
+	glBindVertexArray(duckVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, duckVBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, duckEBO);
+	int indexArraySize;
+	glGetBufferParameteriv(GL_ELEMENT_ARRAY_BUFFER, GL_BUFFER_SIZE, &indexArraySize);
+	glDrawElements(GL_TRIANGLES, indexArraySize / sizeof(unsigned int), GL_UNSIGNED_INT, 0);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+	glBindVertexArray(0);
+}
 
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
@@ -2260,8 +2376,6 @@ void processInput(GLFWwindow* window)
 
 	}
 }
-
-
 
 
 
