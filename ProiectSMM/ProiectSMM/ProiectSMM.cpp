cd6519b3357f787eecbffc297a4242cee2ce7fbf
VladMirea <mirea.vladandrei1@gmail.com>
2022-05-17 23:33:12 +0300

    Added Skull

--- ProiectSMM/ProiectSMM/ProiectSMM.cpp
+++ ProiectSMM/ProiectSMM/ProiectSMM.cpp
@@ -94,6 +94,7 @@ void renderCrocodile(const Shader& shader);
 void renderChair(const Shader& shader);
 void renderSmallDino(const Shader& shader);
 void renderpedastal(const Shader& shader);
+void renderSkull(const Shader& shader);
 
 //objects
 void renderFloor();
@@ -111,6 +112,7 @@ void renderCrocodile();
 void renderChair();
 void renderSmallDino();
 void renderpedastal();
+void renderSkull();
 
 
 //room
@@ -208,7 +210,7 @@ int main(int argc, char** argv)
 	unsigned int chairTexture = CreateTexture(strExePath + "\\chair.jpg");
 	unsigned int smallDino = CreateTexture(strExePath + "\\Ingenia_diffuse.jpg");
 	unsigned int pedastalTexture = CreateTexture(strExePath + "\\wall.jpg");
-
+	unsigned int skullTexture = CreateTexture(strExePath + "\\TriceratopsSkullDiffuse.jpg");
 
 	// configure depth map FBO
 	// -----------------------
@@ -454,6 +456,17 @@ int main(int argc, char** argv)
 		glCullFace(GL_BACK);
 		glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
+		glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
+		glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+		glClear(GL_DEPTH_BUFFER_BIT);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, skullTexture);
+		glEnable(GL_CULL_FACE);
+		glCullFace(GL_FRONT);
+		renderSkull(shadowMappingDepthShader);
+		glCullFace(GL_BACK);
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
 
 		// reset viewport
 		glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
@@ -595,6 +608,13 @@ int main(int argc, char** argv)
 		glBindTexture(GL_TEXTURE_2D, depthMap);
 		glDisable(GL_CULL_FACE);
 		renderpedastal(shadowMappingShader);
+
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, skullTexture);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, depthMap);
+		glDisable(GL_CULL_FACE);
+		renderSkull(shadowMappingShader);
 
 
 		//end
@@ -3037,6 +3057,107 @@ void renderpedastal()
 	glDrawArrays(GL_TRIANGLES, 0, 36);
 	glBindVertexArray(0);
 }
+
+void renderSkull(const Shader& shader)
+{
+	//skull
+
+	glm::mat4 model;
+	model = glm::mat4();
+	model = glm::translate(model, glm::vec3(-60.5f, 5.6f, 0.f));
+	model = glm::scale(model, glm::vec3(2.f));
+	model = glm::rotate(model, glm::radians(125.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+	model = glm::rotate(model, glm::radians(20.0f), glm::vec3(0.0f, 0.0f, -1.0f));
+	shader.SetMat4("model", model);
+	renderSkull();
+}
+
+
+unsigned int indicesE[72000];
+objl::Vertex verE[82000];
+
+GLuint skullVAO, skullVBO, skullEBO;
+
+void renderSkull()
+{
+	// initialize (if necessary)
+	if (skullVAO == 0)
+	{
+
+		std::vector<float> verticess;
+		std::vector<float> indicess;
+
+
+
+		Loader.LoadFile("..\\OBJ\\uploads_files_2791683_TriceratopsSkull.obj");
+		objl::Mesh curMesh = Loader.LoadedMeshes[0];
+		int size = curMesh.Vertices.size();
+		objl::Vertex v;
+		for (int j = 0; j < curMesh.Vertices.size(); j++)
+		{
+			v.Position.X = (float)curMesh.Vertices[j].Position.X;
+			v.Position.Y = (float)curMesh.Vertices[j].Position.Y;
+			v.Position.Z = (float)curMesh.Vertices[j].Position.Z;
+			v.Normal.X = (float)curMesh.Vertices[j].Normal.X;
+			v.Normal.Y = (float)curMesh.Vertices[j].Normal.Y;
+			v.Normal.Z = (float)curMesh.Vertices[j].Normal.Z;
+			v.TextureCoordinate.X = (float)curMesh.Vertices[j].TextureCoordinate.X;
+			v.TextureCoordinate.Y = (float)curMesh.Vertices[j].TextureCoordinate.Y;
+
+
+			verE[j] = v;
+		}
+		for (int j = 0; j < verticess.size(); j++)
+		{
+			vertices[j] = verticess.at(j);
+		}
+
+		for (int j = 0; j < curMesh.Indices.size(); j++)
+		{
+
+			indicess.push_back((float)curMesh.Indices[j]);
+
+		}
+		for (int j = 0; j < curMesh.Indices.size(); j++)
+		{
+			indicesE[j] = indicess.at(j);
+		}
+
+		glGenVertexArrays(1, &skullVAO);
+		glGenBuffers(1, &skullVBO);
+		glGenBuffers(1, &skullEBO);
+		// fill buffer
+		glBindBuffer(GL_ARRAY_BUFFER, skullVBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(verE), verE, GL_DYNAMIC_DRAW);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, skullEBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicesE), &indicesE, GL_DYNAMIC_DRAW);
+		// link vertex attributes
+		glBindVertexArray(skullVAO);
+		glEnableVertexAttribArray(0);
+
+
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+		glEnableVertexAttribArray(2);
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindVertexArray(0);
+	}
+	// render Cube
+	glBindVertexArray(skullVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, skullVBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, skullEBO);
+	int indexArraySize;
+	glGetBufferParameteriv(GL_ELEMENT_ARRAY_BUFFER, GL_BUFFER_SIZE, &indexArraySize);
+	glDrawElements(GL_TRIANGLES, indexArraySize / sizeof(unsigned int), GL_UNSIGNED_INT, 0);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+	glBindVertexArray(0);
+}
+
+
+
 
 
 
