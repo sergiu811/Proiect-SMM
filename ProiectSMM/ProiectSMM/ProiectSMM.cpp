826a590e0bf955c1f10085bc088044203e42c240
robertomodorcea <103879004+robertomodorcea@users.noreply.github.com>
2022-05-17 22:58:37 +0300

    Added Chairs

--- ProiectSMM/ProiectSMM/ProiectSMM.cpp
+++ ProiectSMM/ProiectSMM/ProiectSMM.cpp
@@ -91,6 +91,7 @@ void renderStork(const Shader& shader);
 void renderDuck(const Shader& shader);
 void renderColumn(const Shader& shader);
 void renderCrocodile(const Shader& shader);
+void renderChair(const Shader& shader);
 
 //objects
 void renderFloor();
@@ -105,6 +106,8 @@ void renderStork();
 void renderDuck();
 void renderColumn();
 void renderCrocodile();
+void renderChair();
+
 
 //room
 void renderWall1();
@@ -198,6 +201,7 @@ int main(int argc, char** argv)
 	unsigned int duckTexture = CreateTexture(strExePath + "\\duck.jpg");
 	unsigned int columnTexture = CreateTexture(strExePath + "\\wall.jpg");
 	unsigned int crocodile = CreateTexture(strExePath + "\\Black_Caiman_Crocodile_diff.jpg");
+	unsigned int chairTexture = CreateTexture(strExePath + "\\chair.jpg");
 
 
 
@@ -413,6 +417,18 @@ int main(int argc, char** argv)
 		glCullFace(GL_BACK);
 		glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
+		glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
+		glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+		glClear(GL_DEPTH_BUFFER_BIT);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, chairTexture);
+		glEnable(GL_CULL_FACE);
+		glCullFace(GL_FRONT);
+		renderChair(shadowMappingDepthShader);
+		glCullFace(GL_BACK);
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+
 		// reset viewport
 		glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -532,6 +548,14 @@ int main(int argc, char** argv)
 		glBindTexture(GL_TEXTURE_2D, depthMap);
 		glDisable(GL_CULL_FACE);
 		renderCrocodile(shadowMappingShader);
+
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, chairTexture);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, depthMap);
+		glDisable(GL_CULL_FACE);
+		renderChair(shadowMappingShader);
+
 
 		//end
 
@@ -901,6 +925,26 @@ void renderCrocodile(const Shader& shader)
 	shader.SetMat4("model", model);
 	renderCrocodile();
 }
+
+void renderChair(const Shader& shader)
+{
+	//chair
+	glm::mat4 model;
+	model = glm::mat4();
+	model = glm::translate(model, glm::vec3(-15.f, -0.5f, -15.4f));
+	model = glm::scale(model, glm::vec3(0.012f));
+	model = glm::rotate(model, glm::radians(180.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+	shader.SetMat4("model", model);
+	renderChair();
+
+	model = glm::mat4();
+	model = glm::translate(model, glm::vec3(-15.f, -0.5f, 12.4f));
+	model = glm::scale(model, glm::vec3(0.012f));
+	model = glm::rotate(model, glm::radians(180.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+	shader.SetMat4("model", model);
+	renderChair();
+}
+
 
 
 
@@ -2641,6 +2685,89 @@ void renderCrocodile()
 	glBindVertexArray(crocodileVAO);
 	glBindBuffer(GL_ARRAY_BUFFER, crocodileVBO);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, crocodileEBO);
+	int indexArraySize;
+	glGetBufferParameteriv(GL_ELEMENT_ARRAY_BUFFER, GL_BUFFER_SIZE, &indexArraySize);
+	glDrawElements(GL_TRIANGLES, indexArraySize / sizeof(unsigned int), GL_UNSIGNED_INT, 0);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+	glBindVertexArray(0);
+}
+
+unsigned int indicesP[720000];
+objl::Vertex verP[82000];
+
+GLuint chairVAO, chairVBO, chairEBO;
+
+void renderChair()
+{
+	// initialize (if necessary)
+	if (chairVAO == 0)
+	{
+
+		std::vector<float> verticess;
+		std::vector<float> indicess;
+
+
+
+		Loader.LoadFile("..\\OBJ\\poly_chair.obj");
+		objl::Mesh curMesh = Loader.LoadedMeshes[0];
+		int size = curMesh.Vertices.size();
+		objl::Vertex v;
+		for (int j = 0; j < curMesh.Vertices.size(); j++)
+		{
+			v.Position.X = (float)curMesh.Vertices[j].Position.X;
+			v.Position.Y = (float)curMesh.Vertices[j].Position.Y;
+			v.Position.Z = (float)curMesh.Vertices[j].Position.Z;
+			v.Normal.X = (float)curMesh.Vertices[j].Normal.X;
+			v.Normal.Y = (float)curMesh.Vertices[j].Normal.Y;
+			v.Normal.Z = (float)curMesh.Vertices[j].Normal.Z;
+			v.TextureCoordinate.X = (float)curMesh.Vertices[j].TextureCoordinate.X;
+			v.TextureCoordinate.Y = (float)curMesh.Vertices[j].TextureCoordinate.Y;
+
+
+			verP[j] = v;
+		}
+		for (int j = 0; j < verticess.size(); j++)
+		{
+			vertices[j] = verticess.at(j);
+		}
+
+		for (int j = 0; j < curMesh.Indices.size(); j++)
+		{
+
+			indicess.push_back((float)curMesh.Indices[j]);
+
+		}
+		for (int j = 0; j < curMesh.Indices.size(); j++)
+		{
+			indicesP[j] = indicess.at(j);
+		}
+
+		glGenVertexArrays(1, &chairVAO);
+		glGenBuffers(1, &chairVBO);
+		glGenBuffers(1, &chairEBO);
+		// fill buffer
+		glBindBuffer(GL_ARRAY_BUFFER, chairVBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(verP), verP, GL_DYNAMIC_DRAW);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, chairEBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicesP), &indicesP, GL_DYNAMIC_DRAW);
+		// link vertex attributes
+		glBindVertexArray(chairVAO);
+		glEnableVertexAttribArray(0);
+
+
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+		glEnableVertexAttribArray(2);
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindVertexArray(0);
+	}
+	// render Cube
+	glBindVertexArray(chairVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, chairVBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, chairEBO);
 	int indexArraySize;
 	glGetBufferParameteriv(GL_ELEMENT_ARRAY_BUFFER, GL_BUFFER_SIZE, &indexArraySize);
 	glDrawElements(GL_TRIANGLES, indexArraySize / sizeof(unsigned int), GL_UNSIGNED_INT, 0);
