af220e7f9980c7b00a5f1a0c5aef1e875c7da562
robertomodorcea <103879004+robertomodorcea@users.noreply.github.com>
2022-05-17 23:10:01 +0300

    Added Small Dinosaur

--- ProiectSMM/ProiectSMM/ProiectSMM.cpp
+++ ProiectSMM/ProiectSMM/ProiectSMM.cpp
@@ -92,6 +92,7 @@ void renderDuck(const Shader& shader);
 void renderColumn(const Shader& shader);
 void renderCrocodile(const Shader& shader);
 void renderChair(const Shader& shader);
+void renderSmallDino(const Shader& shader);
 
 //objects
 void renderFloor();
@@ -107,6 +108,7 @@ void renderDuck();
 void renderColumn();
 void renderCrocodile();
 void renderChair();
+void renderSmallDino();
 
 
 //room
@@ -202,6 +204,7 @@ int main(int argc, char** argv)
 	unsigned int columnTexture = CreateTexture(strExePath + "\\wall.jpg");
 	unsigned int crocodile = CreateTexture(strExePath + "\\Black_Caiman_Crocodile_diff.jpg");
 	unsigned int chairTexture = CreateTexture(strExePath + "\\chair.jpg");
+	unsigned int smallDino = CreateTexture(strExePath + "\\Ingenia_diffuse.jpg");
 
 
 
@@ -428,6 +431,17 @@ int main(int argc, char** argv)
 		glCullFace(GL_BACK);
 		glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
+		glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
+		glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+		glClear(GL_DEPTH_BUFFER_BIT);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, smallDino);
+		glEnable(GL_CULL_FACE);
+		glCullFace(GL_FRONT);
+		renderSmallDino(shadowMappingDepthShader);
+		glCullFace(GL_BACK);
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
 
 		// reset viewport
 		glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
@@ -555,6 +569,14 @@ int main(int argc, char** argv)
 		glBindTexture(GL_TEXTURE_2D, depthMap);
 		glDisable(GL_CULL_FACE);
 		renderChair(shadowMappingShader);
+
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, smallDino);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, depthMap);
+		glDisable(GL_CULL_FACE);
+		renderSmallDino(shadowMappingShader);
+
 
 
 		//end
@@ -943,6 +965,20 @@ void renderChair(const Shader& shader)
 	model = glm::rotate(model, glm::radians(180.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 	shader.SetMat4("model", model);
 	renderChair();
+}
+
+void renderSmallDino(const Shader& shader)
+{
+	//smallDino
+
+	glm::mat4 model;
+	model = glm::mat4();
+	model = glm::translate(model, glm::vec3(-33.5f, 0.0f, -13.4f));
+	model = glm::scale(model, glm::vec3(0.14f));
+	model = glm::rotate(model, glm::radians(270.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	//model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+	shader.SetMat4("model", model);
+	renderSmallDino();
 }
 
 
@@ -2774,6 +2810,89 @@ void renderChair()
 	glDrawArrays(GL_TRIANGLES, 0, 36);
 	glBindVertexArray(0);
 }
+
+unsigned int indicesDT2[720000];
+objl::Vertex verDT2[900000];
+
+GLuint smallDinoVAO, smallDinoVBO, smallDinoEBO;
+void renderSmallDino()
+{
+	// initialize (if necessary)
+	if (smallDinoVAO == 0)
+	{
+
+		std::vector<float> verticess;
+		std::vector<float> indicess;
+
+
+
+		Loader.LoadFile("..\\OBJ\\13633_Ingenia_v1_L2.obj");
+		objl::Mesh curMesh = Loader.LoadedMeshes[0];
+		int size = curMesh.Vertices.size();
+		objl::Vertex v;
+		for (int j = 0; j < curMesh.Vertices.size(); j++)
+		{
+			v.Position.X = (float)curMesh.Vertices[j].Position.X;
+			v.Position.Y = (float)curMesh.Vertices[j].Position.Y;
+			v.Position.Z = (float)curMesh.Vertices[j].Position.Z;
+			v.Normal.X = (float)curMesh.Vertices[j].Normal.X;
+			v.Normal.Y = (float)curMesh.Vertices[j].Normal.Y;
+			v.Normal.Z = (float)curMesh.Vertices[j].Normal.Z;
+			v.TextureCoordinate.X = (float)curMesh.Vertices[j].TextureCoordinate.X;
+			v.TextureCoordinate.Y = (float)curMesh.Vertices[j].TextureCoordinate.Y;
+
+
+			verDT2[j] = v;
+		}
+		for (int j = 0; j < verticess.size(); j++)
+		{
+			vertices[j] = verticess.at(j);
+		}
+
+		for (int j = 0; j < curMesh.Indices.size(); j++)
+		{
+
+			indicess.push_back((float)curMesh.Indices[j]);
+
+		}
+		for (int j = 0; j < curMesh.Indices.size(); j++)
+		{
+			indicesDT2[j] = indicess.at(j);
+		}
+
+		glGenVertexArrays(1, &smallDinoVAO);
+		glGenBuffers(1, &smallDinoVBO);
+		glGenBuffers(1, &smallDinoEBO);
+		// fill buffer
+		glBindBuffer(GL_ARRAY_BUFFER, smallDinoVBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(verDT2), verDT2, GL_DYNAMIC_DRAW);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, smallDinoEBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicesDT2), &indicesDT2, GL_DYNAMIC_DRAW);
+		// link vertex attributes
+		glBindVertexArray(smallDinoVAO);
+		glEnableVertexAttribArray(0);
+
+
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+		glEnableVertexAttribArray(2);
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindVertexArray(0);
+	}
+	// render Cube
+	glBindVertexArray(smallDinoVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, smallDinoVBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, smallDinoEBO);
+	int indexArraySize;
+	glGetBufferParameteriv(GL_ELEMENT_ARRAY_BUFFER, GL_BUFFER_SIZE, &indexArraySize);
+	glDrawElements(GL_TRIANGLES, indexArraySize / sizeof(unsigned int), GL_UNSIGNED_INT, 0);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+	glBindVertexArray(0);
+}
+
 
 
 
